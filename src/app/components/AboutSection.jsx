4c7889eefe098a03bf17fce2b50212f631ed057c
Abd-El-RahmanHemida <abd.elrahman.hemida2@gmail.com>
2025-03-25 15:13:25 +0200

    init

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,103 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Next.js</li>
+        <li>React</li>
+        <li>TypeScript</li>
+        <li>prisma</li>
+        <li>Node.js</li>
+        <li>express.js</li>
+        <li>PostgreSQL</li>
+        <li>tailwindcss</li>
+        <li>MongoDB</li>
+        <li>API</li>
+        <li>c++</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>udacity nondegree Advanced Full-Stack Web Development</li>
+        <li>Damnhour University
+        Bachelor&apos;s degree, Computer science</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Advanced Full-Stack Web Development</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} alt="about image"/>
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          I am a full stack web developer with a passion for building interactive, responsive web applications.
+          My technical skills include JavaScript, React, Redux, Next.js, Node.js, Express, PostgreSQL,
+          prisma, MongoDB, and Git. I am a quick learner, constantly seeking new challenges to grow my 
+          knowledge and abilities. As a collaborative team player, I thrive on creating impactful applications 
+          with others and delivering high-quality solutions.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
